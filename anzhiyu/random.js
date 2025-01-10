0a6f9b0ea45f8adf337630faa198d9d207ea5ffc
WMY-WX <1755027422@qq.com>
2025-01-10 22:24:59 +0800

    Site updated: 2025-01-10 22:24:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/06/hello-world/"];function toRandomPost(){
+var posts=["2025/01/10/Markdown语法教学/","2025/01/06/hello-world/","2025/01/10/test1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
