426098ba4075bf3ff66ec6b97fcc3bf07ce147d3
WMY-WX <1755027422@qq.com>
2025-01-15 21:09:58 +0800

    Site updated: 2025-01-15 21:09:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/10/Markdown语法教学/","2025/01/06/hello-world/","2025/01/10/test1/"];function toRandomPost(){
+var posts=["2025/01/15/Markdown-完整教程/","2025/01/10/Markdown-语法教学/","2025/01/06/hello-world/","2025/01/10/test1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
